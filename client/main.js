44567e11977aaef6ab754cb9cae5b9e570d0c6b5
Curtat Laurent <laurent.curtat-prestataire@laposte.fr>
2018-04-27 14:23:19 +0200

    New way of playin sound

--- client/main.js
+++ client/main.js
@@ -4,22 +4,6 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.player.onCreated(function playerOnCreated() {
-  this.audioContext = new AudioContext();
-  this.buffer = null;
-
-  const audioUrl = "public/audio/part1.mp3";
-  let request = new XMLHttpRequest();
-  request.responseType = "arrayBuffer";
-  request.addEventListener("load", () => {
-    if (request.status == 200) {
-      this.audioContext.decodeAudioData(request.response, (buffer) => {
-        this.buffer = buffer;
-      });
-    }
-  });
-
-  request.open("GET", audioUrl, true);
-  request.send();
 
 });
 
@@ -30,11 +14,30 @@ Template.player.helpers({
 });
 
 Template.player.events({
-  'click #play'(event, instance) {
-    console.info("Playin'");
-    const sourceNode = instance.audioContext.createBufferSource();
-    sourceNode.buffer = instance.buffer;
-    sourceNode.connect(instance.audioContext.destination);
-    sourceNode.start(0);
+  'durationchange #audio': function (e, template) {
+    var audio;
+    audio = template.find('#audio');
+    Session.set('duration', audio.duration);
   },
+  'play #audio': function (e, template) {
+    interval = setInterval(function () {
+      var audio;
+      audio = template.find('#audio');
+      Session.set('currentTime', audio.currentTime);      
+    }, 1000); // this is faster then timeupdate but more costly for CPU
+  },
+  'ended #audio': function () {
+    return clearInterval(interval);
+  },
+  'timeupdate #audio': function (e, template) { },
+  'click #play_pause': function (e, template) {
+    let currentState = Session.get("state") ? Session.get("state") : 0;
+    if (currentState) {
+      Session.set("state", 0);      
+      template.find('#audio').pause();
+    } else {      
+      Session.set("state", 1);
+      template.find('#audio').play();
+    }
+  }
 });
